c935439b1ab5aeb4808a472126d67348a047980a
A-Karapetyan <akaryan9991@gmail.com>
2022-07-16 11:58:53 +0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from './http/http.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [HttpService]
 })
-export class AppComponent {
-  title = 'task-casino-page';
+export class AppComponent implements OnInit {
+  constructor(private httpService: HttpService) {
+
+  }
+  ngOnInit(): void {
+    this.httpService.get('games.php').subscribe(res => console.log(res));
+  }
+
+
 }
---
+++ src/app/http/http.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+
+@Injectable()
+export class HttpService {
+
+	constructor(
+		private http: HttpClient,
+	) { }
+
+  public get(url: string, params?: HttpParams | any, headers?: HttpHeaders): Observable<any> {
+		return this.http.get(this.getFullUrl(url), { params, headers });
+	}
+
+	public post(url: string, params?: HttpParams | any, headers?: HttpHeaders): Observable<any> {
+		return this.http.get(this.getFullUrl(url), { params, headers });
+	}
+
+	public put(url: string, params?: HttpParams | any, headers?: HttpHeaders): Observable<any> {
+		return this.http.get(this.getFullUrl(url), { params, headers });
+	}
+
+	public delete(url: string, params?: HttpParams | any, headers?: HttpHeaders): Observable<any> {
+		return this.http.get(this.getFullUrl(url), { params, headers });
+	}
+
+  private getFullUrl(url: string) {
+    return environment.baseUrl + url;
+  }
+}
