4f6de981bacc1047d9b7606bd705f5acf85107ef
A-Karapetyan <akaryan9991@gmail.com>
2022-07-21 23:53:37 +0400

    async pipe fix

--- src/app/home/components/game-card/game-card.component.ts
+++ src/app/home/components/game-card/game-card.component.ts
@@ -1,5 +1,5 @@
 import { ChangeDetectorRef, Component, Input, OnDestroy, OnInit } from '@angular/core';
-import { map, Subscription } from 'rxjs';
+import { map, Observable, Subscription } from 'rxjs';
 import { JackpotService } from 'src/app/api/jackpot/jackpot.service';
 import { CategoryItem } from 'src/app/models/category-item.model';
 import { GameListModel } from 'src/app/models/game-list.model';
@@ -10,34 +10,22 @@ import { JackpotListModel } from 'src/app/models/jackpot-list.model';
   templateUrl: './game-card.component.html',
   styleUrls: ['./game-card.component.scss'],
 })
-export class GameCardComponent implements OnInit, OnDestroy{
+export class GameCardComponent implements OnInit {
   @Input() game!: GameListModel;
   @Input() selectedCategory!: CategoryItem;
 
-  private subscription: Subscription;
+  jackpot$: Observable<JackpotListModel>;
 
-  get hasJackpot() {
-    return !!this.game?.jackpot;
-  }
-
-  constructor(private jackpotService: JackpotService, private cdr: ChangeDetectorRef) { }
-  ngOnDestroy(): void {
-    if (this.subscription) {
-      this.subscription.unsubscribe();
-    }
-  }
+  constructor(private jackpotService: JackpotService) { }
 
   ngOnInit(): void {
     this.subToJackpotsListChange();
   }
 
   private subToJackpotsListChange() {
-    this.subscription = this.jackpotService.jackpotsList$
+    this.jackpot$ = this.jackpotService.jackpotsList$
       .pipe(
         map((jackpots: JackpotListModel[]) => jackpots?.find(jb => jb.game === this.game?.id))
-      ).subscribe(jb => {
-        this.game.jackpot = jb?.amount || 0;
-        this.cdr.detectChanges();
-      });
+      );
   }
 }
